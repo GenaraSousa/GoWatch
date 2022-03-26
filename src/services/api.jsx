692d84fc538dfeb39ee5ea67a6e9eab6fc94304d
GenaraSousa <genara7sousa@gmail.com>
2022-03-26 10:34:42 -0300

    Ajustes para build

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 //movie/now_playing&language=pt-BR&page=1
-
 export const key = '460c92e61f580f4be7875c0fb969b3af';
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3'
